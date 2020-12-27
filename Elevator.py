61b4e19a86bb27fb5d25cbf6a69b583704e43d03
sirine ayadi <sirineayadi.dhouib@hotmail.com>
2020-12-27 16:23:42 +0100

    class button elevator python

--- Elevator.py
+++ Elevator.py
@@ -1,16 +1,28 @@
-class Elevator_button(object):
+# DEFINE CONSTANTS VALUES 
+WeigthThreshold     = 500      #  Measure Unit : KG / Max wight that elevator can support 
+MaxCapacity         =  10      #  Measure Unit : Person / Max person that elevator can contain
+
+
+
+# DEFINE GLOBAL PARAMETERS 
+Emergency           =   False   #  If Emergency all elevators should be evacuated  
+
+#class that describes button inside the elevator// 
+
+# Elevator_Button class
+
+class ElevatorButton:
+    
+    
+# Properties declaration 
+
     def __init__(self,idbutton,idelevator,status):
-        self.idbutton = idbutton
-        self.idelevator = idelevator
-        self.status = status
-    def setStatus(self,status):
-        self.status = status
-    def getStatus(self):
-        return self.status
-    def __str__(self):
-        return " ID = " + str(self.idbutton) + "\n ID ELEVATOR : " + str(self.idelevator) + "\n Status : " + str(self.status)
+        self.idbutton=idbutton
+        self.idelevator=idelevator
+        self.status=status
+    def setstatus(self,status):
+        self.status=status
+    def getstatus(self):
+        return self.statuts
 
-
-eb = Elevator_button(10,2,"deactivate")
-print(eb)
 
