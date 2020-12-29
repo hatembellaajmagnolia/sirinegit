32e9c9332130464114794351f4ce09ba65bb4909
hatembellaaj <hatem.bellaaj@redcad.org>
2020-12-29 19:33:01 +0100

    Update Elevator.py
    fix calling attribute

--- Elevator.py
+++ Elevator.py
@@ -146,7 +146,7 @@ class shaft :
         # add outside buttons
         self.outsidebuttons=[]
         self.outsidebuttons.append(outsidebutton("UP",0,"DESACTIVATED"))
-        for i in range(1,self.nbfloor-1):
+        for i in range(1,nbfloor-1):
             #instantiate button outside (up or down) Parameters : direction, floor, status
             self.outsidebutton.append(outsidebutton("DOWN",i,"DESACTIVETED"))
             self.outsidebuttons.append(outsidebutton("UP",i,"DESACTIVETED"))
