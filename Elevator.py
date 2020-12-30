b32542d3d1e20c60453a3abf038fc0b7affcd8f2
hatembellaaj <hatem.bellaaj@redcad.org>
2020-12-30 14:57:00 +0100

    Update Elevator.py
    fix abs problem

--- Elevator.py
+++ Elevator.py
@@ -160,11 +160,11 @@ class shaft :
                         self.eligibleElevator.append(elevator)
         
         if len(self.eligiblelevator) > 1 :
-            return self.findnearestelevator(outsidebutton,self.eligiblelevator) 
+            return self.findnearestelevator(outsidebutton.currentfloor,self.eligiblelevator) 
         elif len(self.eligiblelevator) == 1 :
             return self.eligiblelevator[0]
         else:
-            return self.findnearestelevator(outsidebutton,self.elevators)
+            return self.findnearestelevator(outsidebutton.currentfloor,self.elevators)
    
     def findnearestelevator(self,currentfloor,elevatorslist):
         bestelevator = elevatorslist[0]     #lets take the first element of the array and compare it to each elevator1 of the array  
