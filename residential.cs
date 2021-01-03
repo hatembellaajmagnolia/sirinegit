2e19ceb3ca65e32bb3e6d7b8928f863de34e2011
sirine ayadi <sirineayadi.dhouib@hotmail.com>
2021-01-03 23:23:11 +0100

    declaration variable

---
+++ residential.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Namespace {
+    public  class Module {
+        public static void Main(string[] args)
+    {
+       const int WeigthThreshold = 500;
+       const int  MaxCapacity = 10;
+        const int  strElevator = "Elevator: ";
+        const var Emergency = false;
+        
+
+
+		
+        
